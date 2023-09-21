6db79563271a2ee2e9f44b1950ac83af7919605d
ashish malla <74368820+im45145v@users.noreply.github.com>
2023-09-21 23:06:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,27 +4,28 @@ from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
 
-st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title="Chat with the NiyamBhodak", page_icon="⛏️", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
-st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
-st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
+st.title("Chat with the NiyamBhodak 💬⛏️")
+st.info("A Chatbot to help with various Acts, Rules, and Regulations applicable to Mining industries
+", icon="🤖")
          
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+        {"role": "NiyamBhodak", "content": "Ask me a question about Mining Industry!"}
     ]
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+    with st.spinner(text="Give me 1-2 minutes to tokenize things ."):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Indian mining sector and your job is to answer  questions related to various Acts, Rules, and Regulations applicable to Mining industries. Assume that all questions are related to the .Indian Mining industries. Keep your answers True and based on facts – do not hallucinate features."))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
 index = load_data()
-# chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features.")
+# chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True, system_prompt="You are an expert on the Indian mining sector and your job is to answer  questions related to various Acts, Rules, and Regulations applicable to Mining industries. Assume that all questions are related to the .Indian Mining industries. Keep your answers True and based on facts – do not hallucinate features.")
 chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
@@ -35,10 +36,10 @@ for message in st.session_state.messages: # Display the prior chat messages
         st.write(message["content"])
 
 # If last message is not from assistant, generate a new response
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
+if st.session_state.messages[-1]["role"] != "NiyamBhodak":
+    with st.chat_message("NiyamBhodak"):
         with st.spinner("Thinking..."):
             response = chat_engine.chat(prompt)
             st.write(response.response)
-            message = {"role": "assistant", "content": response.response}
+            message = {"role": "NiyamBhodak", "content": response.response}
             st.session_state.messages.append(message) # Add response to message history
